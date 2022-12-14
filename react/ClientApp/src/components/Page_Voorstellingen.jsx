ccd69d012a5bee24971561d2e7c7364f05891e5a
KingKelpo <kingkelpo163@gmail.com>
2022-12-14 20:28:50 +0100

    tabel

--- react/ClientApp/src/components/Page_Voorstellingen.jsx
+++ react/ClientApp/src/components/Page_Voorstellingen.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import DropdownMenu from './DropdownMenu';
+import ProductBanner from './ProductBanner';
+
 
 function Page_Voorstellingen() {
 
-    {/*deze lijsten zijn de opties die in de dropdownmenus worden getoonds*/}
-    const sortOptions = ["Datum","Kosten","Populariteit"]
-    const filterOptions = ["geen","idee"]
+    {/*deze lijsten zijn de opties die in de dropdownmenus worden getoonds*/ }
+    const sortOptions = ["Datum", "Kosten", "Populariteit"]
+    const filterOptions = ["geen", "idee"]
+    
+    {/*deze lijst moet gevuld worden met de ids van de shows die de mensen die geen donateur zijn
+    	mogen zien, showList.map maakt een productbanner voor elke show */}
+    let showList = [1,2,3]
 
     return (
-        <>
+        <body>
             <div className="flex-container-horizontal">
-                <DropdownMenu type="Sorteer" options={sortOptions}/>
-                <DropdownMenu type="Filter" options={filterOptions}/>
-                <input/>
+                <DropdownMenu type="Sorteer" options={sortOptions} />
+                <DropdownMenu type="Filter" options={filterOptions} />
+                <input />
             </div>
-            
+
             <div className="flex-container-vertical">
+                
+                {showList.map((item) => {
+                    return <ProductBanner id={item} />
+                })}
 
             </div>
 
 
-        </>
+        </body>
     )
 }
 
