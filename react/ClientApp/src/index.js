de16e18868163203b41cd1c1046c5d7f19adde66
AjoebG <ajoeb19044887@gmail.com>
2022-12-19 10:21:35 +0100

    index

--- react/ClientApp/src/index.js
+++ react/ClientApp/src/index.js
@@ -1,9 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
+﻿import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 
 //pagina imports
 import Header from './components/Header'
@@ -11,42 +13,36 @@ import Page_Startpagina from './components/Pages/Page_Startpagina';
 import Page_Voorstellingen from './components/Pages/Page_Voorstellingen';
 import Page_VoorstellingInfo from './components/Pages/Page_VoorstellingInfo';
 import Page_StoelKeuze from "./components/Pages/Page_StoelKeuze";
-import Login from './components/Login-and-Register/Page_Login_Visitors';
-import Register from './components/Login-and-Register/Page_Register_Visitors';
 import Page_ContactGegevens from './components/Pages/Page_ContactGegevens';
 import Page_Toegankelijkheid from './components/Pages/Page_Toegankelijkheid';
 import Page_Begunstigersportaal from './components/Pages/Page_Begunstigersportaal';
-import Page_Betaling from "./components/Pages/Page_Betaling";
-import Page_Betaald from "./components/Pages/Page_Betaald";
-import Page_Winkelmand from "./components/Pages/Page_Winkelmand";
 
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+    <React.StrictMode>
 
-    <Router>
+      <Router>
 
-      <Routes>
-        <Route path='/' element={<Page_Startpagina />} />
-        <Route path='/toegankelijkheid' element={<Page_Toegankelijkheid />} />
-        <Route path='/contactgegevens' element={<Page_ContactGegevens />} />
-        <Route path='/voorstellingen' element={<Page_Voorstellingen />} />
-        <Route path='/voorstellingInfo' element={<Page_VoorstellingInfo/>}/>
-        <Route path='/stoelKeuze' element={<Page_StoelKeuze/>}/>
-        <Route path='/inloggen' element={<Login/>}/>
-        <Route path='/registreren' element={<Register/>}/>
-        <Route path='/betaling' element={<Page_Betaling/>}/>
-        <Route path='/betaald' element={<Page_Betaald/>}/>
-        <Route path='/winkelmand' element={<Page_Winkelmand/>}/>
-        {<Route path='/begunstigersportaal' element={<Page_Begunstigersportaal />} /> /*dit moet nog weggehaald/geautoriseerd worden, het begunstigersportaal moet alleen voor donateurs zijn*/}
-      </Routes>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, requireAuth, ...rest } = route;
+            return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
+          })}
+          <Route path='/' element={<Page_Startpagina />} />
+          <Route path='/toegankelijkheid' element={<Page_Toegankelijkheid />} />
+          <Route path='/contactgegevens' element={<Page_ContactGegevens />} />
+          <Route path='/voorstellingen' element={<Page_Voorstellingen />} />
+          <Route path='/voorstellingInfo' element={<Page_VoorstellingInfo/>}/>
+          <Route path='/stoelKeuze' element={<Page_StoelKeuze/>}/>
+          {<Route path='/begunstigersportaal' element={<Page_Begunstigersportaal />} /> /*dit moet nog weggehaald/geautoriseerd worden, het begunstigersportaal moet alleen voor donateurs zijn*/}
+        </Routes>
 
-    </Router>
+      </Router>
 
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
