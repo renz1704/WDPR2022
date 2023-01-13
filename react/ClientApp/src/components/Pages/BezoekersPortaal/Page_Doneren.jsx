b3ee582f35d7726386be7f68628d16eaa1aeeaf3
Renzo <renz1704@hotmail.nl>
2023-01-13 20:12:48 +0100

    Verder gewerkt aan het donatie paneel, 4 api's gemaakt en o.a. met het toestemmingen gedeelte. + nog een hoop aan de front end en bug fixes. Visitor wordt aangemaakt bij user aanmaken met daarin IdentityUser.

--- react/ClientApp/src/components/Pages/BezoekersPortaal/Page_Doneren.jsx
+++ react/ClientApp/src/components/Pages/BezoekersPortaal/Page_Doneren.jsx
@@ -1,27 +1,82 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import BezoekersPortaalHeader from '../../BezoekersPortaalHeader';
+import "../../../styles/generalStyle.css";
 
 function Page_Doneren() {
+
+    const [email, setEmail] = useState("");
+    const [hideAfterEmail, setHideAfterEmail] = useState(true);
+
+    const donerenKnop = () => {
+
+    }
+
+    useEffect(  () => {
+        
+    // HIER MOET DE FETCH naar de userEmailExists in de donationcontroller, eerst checken of de api werkt.
+        // dan kan hier komen als de email er is, kan het doneren paneel te voorschijn komen.
+    }, []);
+
+
+    const permissionButton = () => {
+
+        
+        setHideAfterEmail(false);
+        console.log(email);
+        
+    }
+
+
+
 
     return (
         <>
             <BezoekersPortaalHeader />
             <div>
                 Wilt u Theater Laak steunen door ons te doneren?
-                Vanaf 1000 euro aan donaties krijgt u toegang tot ons "Begunstigersportaal". 
+                Vanaf 1000 euro aan donaties krijgt u toegang tot ons "Begunstigersportaal".
                 <p>
-                Hier kunt u voorstellingen zien en "pre orderen" nog voordat ze uitkomen.
+                    Hier kunt u voorstellingen zien en "pre orderen" nog voordat ze uitkomen.
                 </p>
+                <div></div>
                 <p>
-                    De donaties worden gedaan via het bekende platform "IkDoneer".
+                    De donaties worden gedaan via het bekende platform "IkDoneer". U zal hiervoor eerst toestemming moeten geven met de onderstaande link:
                 </p>
-                <button>Doneren</button>
+                <form className="permissionForm" onSubmit={(e) => e.preventDefault()}>
+                    <label>Vul hier uw email-adres in: </label>
+                    <input
+                        required="Email is verplicht."
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></input>
+                    <button
+                        type="onSubmit"
+                        disabled={email == ""}
+                        className="permission"
+                        onClick={permissionButton}
+                    >
+                        Email invoeren
+                    </button>
+                </form>
+                <div className={`AfterEmail ${hideAfterEmail ? "hide-AfterEmail" : ""}`}>
+
+
+
+                    <button>Doneren</button>
+
+                    <div>
+                        <hr></hr>
+                        <div>
+                            Wilt u Theater Laak toegang geven tot uw donaties bij IkDoneer? Wij kunnen u bij het juiste bedrag dan toegang geven tot ons begunstigersportaal.
+                        </div>
+
+                        <a target="_blank" href="https://ikdoneer.azurewebsites.net//Toegang?url=https://localhost:7293/api/Donation/addtokenuser" onClick={permissionButton}>Druk hier om toestemming te verlenen.</a>
+                    </div>
+                </div>
             </div>
-            <div>
-                Wilt u Theater Laak toegang geven tot uw donaties bij IkDoneer? Wij kunnen u bij het juiste bedrag dan toegang geven tot ons begunstigersportaal.
-            </div>
-            <a target="_blank" href="https://ikdoneer.azurewebsites.net//Toegang?url=https://localhost:44419/api/donation/addTokenUser%3A%2F%2F%2F">Druk hier om toestemming te verlenen.</a>
         </>
+
+
     )
 
 }
--- react/Controllers/UserController.cs
+++ react/Controllers/UserController.cs
@@ -19,11 +19,13 @@ namespace react.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        TheaterDbContext _context;
 
-        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, TheaterDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
         // [HttpPost]
         // [Route("login")]
@@ -41,30 +43,31 @@ namespace react.Controllers
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDTO model){
-                var _user = await _userManager.FindByNameAsync(model.Email);
-    if (_user != null)
-        if (await _userManager.CheckPasswordAsync(_user, model.Password))
+        public async Task<IActionResult> Login([FromBody] LoginDTO model)
         {
-            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"));
+            var _user = await _userManager.FindByNameAsync(model.Email);
+            if (_user != null)
+                if (await _userManager.CheckPasswordAsync(_user, model.Password))
+                {
+                    var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"));
 
-            var signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
-            var claims = new List<Claim> { new Claim(ClaimTypes.Name, _user.Email) };
-            var roles = await _userManager.GetRolesAsync(_user);
-            foreach (var role in roles)
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            var tokenOptions = new JwtSecurityToken
-            (
-                issuer: "https://localhost:7293",
-                audience: "https://localhost:7293",
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(10),
-                signingCredentials: signingCredentials
-            );
-            return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(tokenOptions) });
-        }
+                    var signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+                    var claims = new List<Claim> { new Claim(ClaimTypes.Name, _user.Email) };
+                    var roles = await _userManager.GetRolesAsync(_user);
+                    foreach (var role in roles)
+                        claims.Add(new Claim(ClaimTypes.Role, role));
+                    var tokenOptions = new JwtSecurityToken
+                    (
+                        issuer: "https://localhost:7293",
+                        audience: "https://localhost:7293",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(10),
+                        signingCredentials: signingCredentials
+                    );
+                    return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(tokenOptions) });
+                }
 
-    return Unauthorized();
+            return Unauthorized();
         }
         [HttpPost]
         [Route("registreer")]
@@ -73,25 +76,45 @@ namespace react.Controllers
             var user = new IdentityUser { UserName = model.Email, Email = model.Email };
 
             var resultaat = await _userManager.CreateAsync(user, model.Password);
+            var addToVisitor = new Visitor();
+            addToVisitor.IdentityUser = user;
+            _context.Visitors.Add(addToVisitor);
+            _context.SaveChanges();
 
             return !resultaat.Succeeded ? new BadRequestObjectResult(resultaat) : StatusCode(201);
 
         }
 
         [HttpGet]
-        [Route("getUser/{id}")]
-        public async Task<ActionResult<string>> GetUserId(String id)
+        [Route("userExists/{email}")]
+        public async Task<ActionResult<String>> userExists(String email)
         {
+
+            var newuser = new IdentityUser { UserName = "hallo@hallo.nl", Email = "hallo@hallo.nl" };
+
+            var resultaat = await _userManager.CreateAsync(newuser, "Hallo123!");
+            var addToVisitor = new Visitor();
+            addToVisitor.IdentityUser = newuser;
+
+            _context.Visitors.Add(addToVisitor);
+            _context.SaveChanges();
+
+            var user = _context.Visitors.Any(x => x.IdentityUser.Email == email);
+            Console.WriteLine(user);
             // Check if the user exists in the database
-            var user = await _userManager.Users.SingleOrDefaultAsync(x => x.Id == id);
-            if (user == null)
+            if (user == false)
             {
                 return NotFound();
             }
 
-            // Return the user ID
-            return user.Id;
+            // Return the email
+            return email;
         }
+
+        //DEZE FIXEN! 
+
+        //email checken of hij bestaat en dan opjecten showen in react.
+
 
 
 
@@ -113,6 +136,6 @@ namespace react.Controllers
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string donationToken{get; set;}
+        public string donationToken { get; set; }
     }
 }
