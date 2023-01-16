f8b348954efdcb6e08f3537dbfc20f53d3eac7fd
KingKelpo <kingkelpo163@gmail.com>
2023-01-16 21:26:03 +0100

    Groupcontroller tests

---
+++ Tests_API/GroupControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Tests_API;
+
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Moq;
+
+public class GroupControllerTests
+{
+    [Fact]
+    public async Task CreateGroup_ReturnsGroup()
+    {
+        // Arrange
+        var group = new Group { Id = 1, Name = "Test Group" };
+
+        var mockDbContext = new Mock<ITheaterDbContext>();
+        mockDbContext.Setup(x => x.Groups.Add(It.IsAny<Group>())).Callback<Group>((group) => {});
+        var controller = new GroupController(mockDbContext.Object);
+
+        // Act
+        var result = await controller.createGroup(group);
+
+        // Assert
+        var returnGroup = Assert.IsType<Group>(result.Value);
+        Assert.Equal(group, returnGroup);
+
+    }
+}
